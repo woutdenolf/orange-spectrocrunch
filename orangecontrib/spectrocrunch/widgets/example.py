6300f0b877fbbee99d4bf61473fb2a20d98e6493
woutdenolf <woutdenolf@users.sf.net>
2019-02-03 18:05:45 +0100

    [gui] example settings form

--- orangecontrib/spectrocrunch/widgets/example.py
+++ orangecontrib/spectrocrunch/widgets/example.py
@@ -1,6 +1,7 @@
 from Orange.widgets.widget import OWWidget
 from Orange.widgets.settings import Setting
 from . import widgetutils
+from Orange.widgets import gui
 
 class Widget(OWWidget):
     # Widget needs a name, or it is considered an abstract widget
@@ -8,37 +9,19 @@ class Widget(OWWidget):
     name = 'Example'
     description = 'Description'
     icon = 'icons/example.svg'
-    want_main_area = False
+    want_basic_layout = False
 
     number = Setting(42)
     string = Setting("test")
 
     def __init__(self):
         super().__init__()
-
-        form = widgetutils.SettingForm(self.controlArea)
-        form.addSetting(self, 'string')
-        form.addSetting(self, 'number')
-
-        #main_layout = QtGui.QVBoxLayout()
-        #self.setLayout(main_layout)
-
-
-        #layout = QtGui.QHBoxLayout()
-        #label = QtGui.QLabel("Line:")
-        #linedit = QtGui.QLineEdit()
-        #horizontalLayout1.addWidget(treelist1)
-        #horizontalLayout1.addWidget(treelist2)
-        #horizontalLayout2.addWidget(label)
-        #horizontalLayout2.addWidget(linedit)
-
-
-        #label = QtGui.QLabel('Hello, World!')
-        #self.controlArea.layout().addWidget(
-        #    label, QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
+        form = widgetutils.SettingForm(parent=self)
+        self.stringWidget = form.addSetting(self, 'string', updateCallback=self.editString)
+        self.numberWidget = form.addSetting(self, 'number', updateCallback=self.editNumber)
 
     def editString(self):
-        print(self.stringWidget.value())
+        print(self.stringWidget.text())
 
     def editNumber(self):
         print(self.numberWidget.value())
--- orangecontrib/spectrocrunch/widgets/widgetutils.py
+++ orangecontrib/spectrocrunch/widgets/widgetutils.py
@@ -1,23 +1,43 @@
-from AnyQt import QtWidgets
+from AnyQt import QtWidgets, QtCore
+from AnyQt.QtCore import Qt
 from Orange.widgets.utils import getdeepattr
 from Orange.widgets.settings import Setting
 import numbers
+import sys
 
 
 class SettingForm(QtWidgets.QWidget):
 
-    def __init__(self, parent=None, margin=0, spacing=-100):
-        super(SettingForm, self).__init__(parent)
-        layout = QtWidgets.QFormLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent=parent)
+        self.initUI(**kwargs)
+
+    def initUIParent(self):
+        parent = self.parent()
+        if parent is None:
+            return
+        layout = parent.layout()
+        if layout is None:
+            layout = QtWidgets.QVBoxLayout()
+            parent.setLayout(layout)
+        layout.addWidget(self)
+
+    def initUI(self, margin=0, spacing=4):
+        self.initUIParent()
+        layout = QtWidgets.QFormLayout()
+        layout.setContentsMargins(margin, margin, margin, margin)
         layout.setSpacing(spacing)
-        self.layout = layout
+        policy = QtWidgets.QFormLayout.AllNonFixedFieldsGrow
+        layout.setFieldGrowthPolicy(policy)
+        #layout.setFormAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        layout.setLabelAlignment(Qt.AlignLeft)
+        self.setLayout(layout)
 
-    def sizeHint(self):
-        width, height = super(SettingForm, self).sizeHint()
-        return QtWidgets.QSize(int(1.8 * width), height)
+    #def sizeHint(self):
+    #    s = super().sizeHint()
+    #    return QtCore.QSize(int(1.5 * s.width()), s.height())
 
-    def addSetting(self, owner, name, label=None):
+    def addSetting(self, owner, name, label=None, updateCallback=None):
         """
         :param owner: setting owner
         :type owner: OWWidget or OWComponent
@@ -34,14 +54,23 @@ class SettingForm(QtWidgets.QWidget):
         if isinstance(value, str):
             field = QtWidgets.QLineEdit()
             field.setText(value)
+            if updateCallback:
+                field.textChanged.connect(updateCallback)
         elif isinstance(value, numbers.Number):
             if isinstance(value, numbers.Integral):
                 field = QtWidgets.QSpinBox()
+                ma = sys.maxsize
+                field.setRange(-ma-1, ma)
             else:
                 field = QtWidgets.QDoubleSpinBox()
             field.setValue(value)
+            if updateCallback:
+                field.valueChanged.connect(updateCallback)
         else:
             raise ValueError('{} does not have a Qt widget'
                              .format(repr(type(value).__qualname__)))
         # Append
-        self.layout.addRow(label, field)
+        policy = QtWidgets.QSizePolicy.Expanding
+        field.setSizePolicy(policy, policy)
+        self.layout().addRow(label, field)
+        return field
